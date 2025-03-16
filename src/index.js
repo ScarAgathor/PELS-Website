cc5fcc5f5dbbd8bc4c27e368aedae92c1f90e662
Edidiong <edidiongdekong@gmail.com>
2025-03-15 19:59:46 -0500

    first commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+let hamburger = document.querySelector('.hamburger');
+let mobileMenu = document.querySelector('.mobileMenu');
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle('active')
+    mobileMenu.classList.toggle('active')
+})
+
